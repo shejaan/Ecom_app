905effef93cfc57167f7cebfd7a974284a8cd8ca
shejaan <142305506+shejaan@users.noreply.github.com>
2024-05-19 19:28:58 +0530

    settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
